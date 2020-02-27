2e2469b6b504055b34bbcf1fdb5306b8a77fe0fe
tzetzo <tzvetanmarinov@yahoo.com>
2020-02-27 19:22:19 +0200

    comments added

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ const taskRouter = require("./routers/task");
 const app = express();
 // const port = process.env.PORT;
 
-//express middleware for authentication with JWT; auto used by all route handlers;
+//express middleware; auto used by all route handlers;
 //needs to be before the route handlers;
 // app.use((req, res, next) => {
 //   res.status(503).send("Site under maintenance. Please come back later");
 // });
 
 app.use(express.json()); //parses the req.body JSON into object inside our path handlers
-app.use(userRouter); //use these routes
+app.use(userRouter); //use these routes; alternative app.use("/api", require("./routers/user"));
 app.use(taskRouter); //use these routes
 
 module.exports = app;
